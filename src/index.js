75a0737f9f09fb9f56305092604c28071251de7a
AK <akueiminyang@cmail.carleton.ca>
2023-04-15 07:35:54 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    buildToDo(e.target.task.value)
+    form.reset()
+  })
 });
+
+
+function buildToDo(todo){
+  let lst = document.createElement('ul')
+  lst.textContent = `${todo}  `
+  let btn = document.createElement('button')
+  document.querySelector('#tasks').appendChild(lst)
+  btn.addEventListener('click',handleDelete)
+  btn.textContent = 'X'
+  lst.appendChild(btn)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
